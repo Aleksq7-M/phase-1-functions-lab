769a10746e11a33b653da965a3225562e08fa179
aleksq7 <morrisalex2001@gmail.com>
2021-08-10 11:12:29 -0600

    commit tree 3aab99656af25676be90fb87cc03530777746962

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(loc){
+    return Math.abs(loc-42);
+}
+
+function distanceFromHqInFeet(loc){
+    return 264*Math.abs(loc-42);
+}
+
+function distanceTravelledInFeet(start,end){
+    return 264*Math.abs(end-start);
+}
+
+function calculatesFarePrice(start,destination){
+    const distance = 264*Math.abs(destination-start);
+    switch (true){
+        case distance<=400:
+            return 0;
+        case distance>400&&distance<=2000:
+            return ((distance-400)*0.02);
+        case distance>2000&&distance<=2500:
+            return 25;
+        case distance>2500:
+            return 'cannot travel that far';
+    }
+}
